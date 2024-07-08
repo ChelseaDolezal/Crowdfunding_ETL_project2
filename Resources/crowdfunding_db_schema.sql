0b3235ced94061f110820db62196236e6519b1cc
ChelseaDolezal <chelseadolezal@gmail.com>
2024-07-08 18:43:13 -0500

    updates

--- Resources/crowdfunding_db_schema.sql
+++ Resources/crowdfunding_db_schema.sql
@@ -9,6 +9,17 @@ CREATE TABLE contacts (
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE subcategory (
+    subcategory_id VARCHAR(150) PRIMARY KEY,
+    category VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE category (
+    category_id VARCHAR(150) PRIMARY KEY,
+    category VARCHAR(100) NOT NULL
+);
+
 CREATE TABLE campaign (
     cf_id SERIAL PRIMARY KEY,
     contact_id SERIAL NOT NULL,
@@ -25,17 +36,11 @@ CREATE TABLE campaign (
     category_id VARCHAR (150) not null,
     subcategory_id VARCHAR(150) not null,
     FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
-    FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+    FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id),
+	FOREIGN KEY (category_id) REFERENCES category(category_id)
 );
 
-CREATE TABLE subcategory (
-    subcategory_id VARCHAR(150) PRIMARY KEY,
-    category VARCHAR(100) NOT NULL
-);
-CREATE TABLE category (
-    category_id VARCHAR(150) PRIMARY KEY,
-    category VARCHAR(100) NOT NULL
-);
+
 
 SELECT * from category; 
 SELECT * from subcategory;
